a941a23d5f3688ea90cfb383ebe02b2a277223e7
ilya <lchrnshv@gmail.com>
2018-12-21 18:35:33 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,39 @@
+function sleep(time: number): Promise<void> {
+  return new Promise(function (resolve) {
+    setTimeout(function() {
+      resolve();
+    }, time);
+  });
+}
+
+export async function attempt(tryCounter: number,
+                              sleepTime: number,
+                              func: Function,
+                              ...params: any[]): Promise<any> {
+
+  return new Promise(async (resolve, reject) => {
+
+    let result = undefined;
+
+    for (let i = tryCounter; i > 0; i--) {
+      try {
+        result = await func(...params);
+        break;
+
+      } catch (e) {
+
+        await sleep(sleepTime);
+
+        if (i <= 1) {
+          reject(e);
+
+          return;
+        }
+
+      }
+    }
+
+    resolve(result);
+  });
+
+}
