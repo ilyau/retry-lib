ac0f8ffab8023dad9e1ebd726fdad89ee1957bf2
ilya <lchrnshv@gmail.com>
2018-12-21 23:55:19 +0300

    tests

--- test/test.js
+++ test/test.js
@@ -3,10 +3,9 @@ var expect = require('chai').expect;
 var attempt = require('../dist/index.js');
 
 async function methodWithError(parameter) {
-    console.log(methodWithError.counter);
     if (methodWithError.counter < 3) {
         methodWithError.counter++;
-        throw new Error('Error');
+        throw new Error('Error' + methodWithError.counter);
     }
 
     return parameter;
@@ -21,7 +20,6 @@ describe('attempt function test', () => {
         const parameter = 'finish';
 
         const result = await attempt.default(5, 200, (e) => {
-            console.log(e.message);
             return true;
         }, methodWithError, parameter);
 
@@ -34,6 +32,24 @@ describe('attempt function test', () => {
         const parameter = 'finish';
 
         const result = await attempt.default(5, 200, undefined, methodWithError, parameter);
+
+        expect(result).to.equal(parameter);
+
+    });
+
+    it('run methodWithError 3', async () => {
+
+        const parameter = 'finish';
+
+        const result = await attempt.default(5, 200, (error) => {
+            if (error.message === 'Error3') {
+                // stop attempts
+                return false;
+            } else {
+                // continue execution
+                return true;
+            }
+        }, methodWithError, parameter);
 
         expect(result).to.equal(parameter);
 
